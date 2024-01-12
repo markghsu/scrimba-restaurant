7961a6c5faa8377c3107f818ddfd845c49885b21
Mark Hsu <mark.g.hsu@gmail.com>
2024-01-11 23:42:42 -0800

    Feat: Add meal discount

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
-const menuInDB = ref(database,'c');
+const menuInDB = ref(database,'menu');
 
 const paymentForm = document.getElementById("payment-form");
 const myOrder = [];
@@ -40,9 +40,10 @@ function addItemToOrder( itemId ) {
     } else {
         console.log(itemId);
         console.log(menuData.find(({id})=>(id === itemId)));
-        const { price, name } = menuData.find(({id})=>(id === itemId)) || {};
+        const { price, name, type } = menuData.find(({id})=>(id === itemId)) || {};
         myOrder.unshift({
             name,
+            type,
             quantity: 1,
             price,
             id: itemId
@@ -58,10 +59,24 @@ function removeItemFromOrder( itemId ) {
     render();
 }
 
-function totalPrice( order = [] ) {
-    return order.reduce((tot,{ quantity, price }) => (
+function totalPrice( order = [], discount = 0 ) {
+    const total = order.reduce((tot,{ quantity, price }) => (
         tot += quantity * price
         ),0);
+    return total * (1 - discount);
+}
+
+function calcDiscount( order = [] ) {
+    // RETURNS A PERCENTAGE DISCOUNT OF 20% ON THE ENTIRE ORDER IF IT IS A "MEAL" -
+    // IF IT HAS A MAIN, SIDE, AND DRINK IN THE SAME ORDER
+    // CURRENTLY THIS IS HARDCODED, COULD ALSO DO A DB LOOKUP FOR DISCOUNTS
+    const types = new Set(['main','side','drink']);
+    order.forEach(({ type }) => { if(type) types.delete(type) });
+    if(types.size === 0) return ({
+            name: 'Meal Discount',
+            amount: 0.2
+        });
+    return {};
 }
 
 function addMenuItemToDB( item = {} ) {
@@ -73,9 +88,10 @@ function addMenuItemToDB( item = {} ) {
 
 /** RENDERING **/
 function render() {
+    const discount = calcDiscount(myOrder);
     document.getElementById('menu-items').innerHTML = getMenuHtml(menuData);
-    document.getElementById('order-items').innerHTML = getOrderHtml(myOrder);
-    document.getElementById('total-price').textContent = '$' + totalPrice(myOrder);
+    document.getElementById('order-items').innerHTML = getOrderHtml(myOrder, discount);
+    document.getElementById('total-price').textContent = '$' + totalPrice(myOrder, discount.amount).toFixed(2);
     if( myOrder.length === 0 ) {
         document.getElementById("order").classList.add('hidden');
     } else {
@@ -91,21 +107,28 @@ function getMenuHtml( itemList = [] ) {
             <div class="menu-item-info">
                 <h3 class="menu-item-name">${name}</h3>
                 <p class="menu-item-ingredients">${ingredients.join(', ')}</p>
-                <p class="menu-item-price">$${price}</p>
+                <p class="menu-item-price">$${price.toFixed(2)}</p>
             </div>
             <button class="add-btn" data-menu-item="${id}" aria-label="Add to Order"></button>
         </li>
-        `)).join('')
+        `)).join('');
 }
 
-function getOrderHtml( order = [] ) {
-    return order.map(({ name, quantity, price, id }) => (`
+function getOrderHtml( order = [], discount = {} ) {
+    const html = order.map(({ name, quantity, price, id }) => (`
         <li class="order-item">
             <p class="order-item-name">${name}${(quantity>1?' x'+quantity:'')}</p>
             <button class="order-remove-btn" data-order-item="${id}">Remove</button>
-            <p class="order-item-price">$${price * quantity}</p>
+            <p class="order-item-price">$${(price * quantity).toFixed(2)}</p>
         </li>
-        `)).join('')
+        `));
+    if( discount.amount ) html.push(`
+        <li class="order-item order-discount">
+            <p class="order-item-name">${discount.name}</p>
+            <p class="order-item-price">-${discount.amount * 100}%</p>
+        </li>
+        `);
+    return html.join('');
 }
 
 /** EVENT LISTENERS **/
